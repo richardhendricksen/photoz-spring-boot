51b9a161b27a84d1da7b2f2d4fea215a650ce4c0
Richard Hendricksen <richard.hendricksen@gmail.com>
2023-03-27 14:00:51 +0200

    initial commit

---
+++ src/main/java/nl/codecontrol/photozsprintboot/PhotozSprintbootApplication.java
@@ -0,0 +1,13 @@
+package nl.codecontrol.photozsprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotozSprintbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotozSprintbootApplication.class, args);
+	}
+
+}
