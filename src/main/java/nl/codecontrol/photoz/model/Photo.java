c303582ddcbd7cba24a9ea9380d3c90ad4f24b5f
Richard Hendricksen <richard.hendricksen@gmail.com>
2023-03-27 15:35:03 +0200

    add crud endpoints

--- src/main/java/nl/codecontrol/photoz/model/Photo.java
+++ src/main/java/nl/codecontrol/photoz/model/Photo.java
@@ -7,6 +7,6 @@ import lombok.Data;
 @AllArgsConstructor
 public class Photo {
 
-    private long id;
+    private Long id;
     private String filename;
 }
--- src/main/java/nl/codecontrol/photoz/rest/PhotozController.java
+++ src/main/java/nl/codecontrol/photoz/rest/PhotozController.java
@@ -1,18 +1,51 @@
 package nl.codecontrol.photoz.rest;
 
 import nl.codecontrol.photoz.model.Photo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Objects.isNull;
 
 @RestController
 public class PhotozController {
 
-    private final List<Photo> db = List.of(new Photo(1, "hello.jpg"));
+    private final Map<Long, Photo> db = new HashMap<>(){{
+        put(1L, new Photo(1L, "hello.jpg"));
+    }};
 
     @GetMapping("/photoz")
-    public List<Photo> get() {
-        return db;
+    public Collection<Photo> get() {
+        return db.values();
+    }
+
+    @GetMapping("/photoz/{id}")
+    public Photo get(@PathVariable long id) {
+        final Photo photo = db.get(id);
+        if (isNull(photo)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return photo;
+    }
+
+    @DeleteMapping("/photoz/{id}")
+    public void delete(@PathVariable long id) {
+        final Photo photo = db.remove(id);
+        if (isNull(photo)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/photoz")
+    public Photo create(@RequestBody Photo photo) {
+
+        final var id = db.keySet().stream().max(Long::compare).get() + 1;
+        photo.setId(id);
+        db.put(photo.getId(), photo);
+        return photo;
     }
 }
